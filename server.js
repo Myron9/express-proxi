c3123ccaa9ef3193bb931338052322278fb16c1d
Ⓜⓨⓡⓞⓝ   マイロン <96477512+Myron5@users.noreply.github.com>
2023-10-28 17:10:34 +0200

    init

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 4000;
+
+app.get("/", async (req, res) => {
+  try {
+    const { link } = req.query;
+    const response = await axios.get(link, { responseType: "arraybuffer" });
+    res.setHeader("Content-Type", response.headers.get("Content-Type"));
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(response.data);
+  } catch {
+    res.status(400).send("Bad request");
+  }
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((error, _, res, __) => {
+  const { status = 500, message = "Server error" } = error;
+  res.status(status).json({ message });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
