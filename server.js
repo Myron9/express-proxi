1b79c0c2849e25c3596b246c600a243986e6e5a4
Ⓜⓨⓡⓞⓝ   マイロン <96477512+Myron5@users.noreply.github.com>
2023-10-28 17:44:28 +0200

    hello-world

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
+// const express = require("express");
+// const axios = require("axios");
+// const app = express();
+// const port = 4000;
+
+// app.get("/image", async (req, res) => {
+//   try {
+//     const { link } = req.query;
+//     const response = await axios.get(link, { responseType: "arraybuffer" });
+//     res.setHeader("Content-Type", response.headers.get("Content-Type"));
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.send(response.data);
+//   } catch {
+//     res.status(400).send("Bad request");
+//   }
+// });
+
+// app.use((_, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
+
+// app.use((error, _, res, __) => {
+//   const { status = 500, message = "Server error" } = error;
+//   res.status(status).json({ message });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
 const express = require("express");
-const axios = require("axios");
 const app = express();
-const port = 4000;
+const port = 3000;
 
-app.get("/image", async (req, res) => {
-  try {
-    const { link } = req.query;
-    const response = await axios.get(link, { responseType: "arraybuffer" });
-    res.setHeader("Content-Type", response.headers.get("Content-Type"));
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(response.data);
-  } catch {
-    res.status(400).send("Bad request");
-  }
-});
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
-app.use((error, _, res, __) => {
-  const { status = 500, message = "Server error" } = error;
-  res.status(status).json({ message });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
