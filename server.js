40dde24c20dfec211f9871a056f973a89dd21dc0
Ⓜⓨⓡⓞⓝ   マイロン <96477512+Myron5@users.noreply.github.com>
2023-10-29 11:45:04 +0100

    add public and cors

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+
+const path = require("node:path");
+const { presentedImgs, getImg4Keys } = require("./config");
+
 const app = express();
 const port = 4000;
+
+app.use(cors());
+app.use(express.static("public"));
 
 app.get("/image", async (req, res) => {
   try {
     const { link } = req.query;
+    if (presentedImgs.includes(link)) {
+      const filePath = path.resolve(__dirname, "public", getImg4Keys[link]);
+      res.sendFile(filePath);
+      return;
+    }
     const response = await axios.get(link, { responseType: "arraybuffer" });
     res.setHeader("Content-Type", response.headers.get("Content-Type"));
-    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(response.data);
   } catch {
     res.status(400).send("Bad request");
